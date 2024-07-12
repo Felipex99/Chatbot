4ee4d5300e19d458ab8dc3066a36c01f75a686e1
Felipe Nunes <felipenuneto@gmail.com>
2024-07-12 04:38:02 -0300

    Problemas de API com o supabase

--- ollama.js
+++ ollama.js
@@ -1,5 +1,10 @@
 import ollama from 'ollama'
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
+import { createClient } from '@supabase/supabase-js'
+
+const SUPABASE_URL = "https://ohitcvvlqfjnnxtacgtb.supabase.co"
+const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oaXRjdnZscWZqbm54dGFjZ3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk5MDU5OTcsImV4cCI6MjAzNTQ4MTk5N30.AVBzGv4JkcK-KDa8toYBi3V_dOA2DWBxX4iWnN2aq68"
+const supabase = createClient(SUPABASE_URL , SUPABASE_API_KEY)
 
 async function splitDocument(path){
     const response = await fetch(path)
@@ -80,3 +85,5 @@ document.addEventListener("DOMContentLoaded", function(){
     })
 })
 
+await supabase.from("text").insert(list_content_embedding)
+console.log("Data Uploaded!!!")
