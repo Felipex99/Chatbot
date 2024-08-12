351068c5525cfe122c5c052c8b6706118d9ad81c
Felipe Nunes <felipenuneto@gmail.com>
2024-08-12 18:25:40 -0300

    Remover Api supabase

--- ollama.js
+++ ollama.js
@@ -2,8 +2,7 @@ import ollama from 'ollama'
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter"
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://ohitcvvlqfjnnxtacgtb.supabase.co"
-const SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oaXRjdnZscWZqbm54dGFjZ3RiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk5MDU5OTcsImV4cCI6MjAzNTQ4MTk5N30.AVBzGv4JkcK-KDa8toYBi3V_dOA2DWBxX4iWnN2aq68"
+
 const supabase = createClient(SUPABASE_URL , SUPABASE_API_KEY)
 
 
